3784ce0546192f91b04d42057733c29fbba7c06b
kimdozzi <neo5188@gmail.com>
2023-03-02 09:13:38 +0900

    명언 앱 1단계 완료

--- src/Test01/Main.java
+++ src/Test01/Main.java
@@ -1,4 +1,14 @@
 package Test01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        String s = br.readLine();
+    }
 }
