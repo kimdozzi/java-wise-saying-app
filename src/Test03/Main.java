a67c56b962655f417f2646c372e81d388e7902f1
kimdozzi <neo5188@gmail.com>
2023-03-02 09:27:40 +0900

    명언 앱 3단계 완료

---
+++ src/Test03/Main.java
@@ -0,0 +1,46 @@
+package Test03;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            명언앱 a명언앱 = new 명언앱();
+            System.out.print("명령) ");
+            String input = br.readLine();
+            if (input.equals("종료")) break;
+            System.out.print("명언 : ");
+            a명언앱.set명언(br.readLine());
+            System.out.print("작가 : ");
+            a명언앱.set작가(br.readLine());
+            System.out.println("1번 명언이 등록되었습니다.");
+        }
+
+    }
+}
+class 명언앱 {
+    private String 명언;
+    private String 작가;
+
+    public String get명언() {
+        return 명언;
+    }
+
+    public void set명언(String 명언) {
+        this.명언 = 명언;
+    }
+
+    public String get작가() {
+        return 작가;
+    }
+
+    public void set작가(String 작가) {
+        this.작가 = 작가;
+    }
+}
